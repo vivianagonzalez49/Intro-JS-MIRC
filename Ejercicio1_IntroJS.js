22cd6a889096ccb4533ae6e29df09eeea3d971a8
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2021-07-14 12:09:23 -0500

    Exercise 1 -  Intro JS MIRC

---
+++ Ejercicio1_IntroJS.js
@@ -0,0 +1,8 @@
+/*Ejercicio 1
+
+Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
+
+Recuerda que para pedirle al usuario que escriba una frase debes utilizar el metodo prompt() y para escribir en la consola debes utilizar el metodo console.log().*/
+
+let phrase = prompt("Por favor digite la frase: ")
+console.log(`La frase es: ${phrase}`)
