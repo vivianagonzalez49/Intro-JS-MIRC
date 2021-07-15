551e96009f063aafb3c1b0ccc65951a1fd7debc4
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2021-07-15 16:16:48 -0500

    Exercise 16 - Intro JS MIRC

---
+++ Ejercicio16_IntroJS.js
@@ -0,0 +1,16 @@
+/* Escribe un programa para la consola que le pida al usuario dos números e imprima en la consola los números en ese rango. Por ejemplo, si el usuario ingresa 5 como primer número y 10 como segundo número, el programa debe imprimir:
+5
+6
+7
+8
+9
+10
+Nota 1: Puedes asumir que el primer número siempre va a ser menor o igual que el segundo.
+Nota 2: Fíjate que se imprimen también los números en los límites, en el ejemplo el 5 y el 10. */
+
+let firstNumber = parseInt(prompt("Por favor digite el primer número: "));
+let secondNumber = parseInt(prompt("Por favor digite el segundo número: "));
+
+for (i = firstNumber; i <= secondNumber; i++) {
+  console.log(i);
+}
