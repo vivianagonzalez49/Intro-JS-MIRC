d97aeb947eacaa722bc120115af209acdcaa3cdf
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2021-07-15 15:40:29 -0500

    Exercise 12 - Intro JS MIRC

---
+++ Ejercicio12_IntroJS.js
@@ -0,0 +1,8 @@
+/* Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces.
+Nota: utiliza un ciclo para imprimir la frase las 10 veces. */
+
+let userPhrase = prompt("Por favor digite la frase: ");
+
+for (let i = 1; i <= 10; i++) {
+  console.log(userPhrase);
+}
