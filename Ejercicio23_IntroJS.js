147cf99b7ee5abeeb9235ea72331f3152731ec20
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2021-07-18 21:49:54 -0500

    Exercise 23 - Intro JS MIRC

---
+++ Ejercicio23_IntroJS.js
@@ -0,0 +1,7 @@
+/* Escribe un programa que le pida una frase al usuario y la repita en mayúsculas. Por ejemplo:
+Ingresa una frase> Hola Mundo
+HOLA MUNDO */
+
+let phrase = prompt('Por favor ingrese una frase: ')
+
+console.log('La frase en mayúscula sostenida es: ', phrase.toUpperCase())
